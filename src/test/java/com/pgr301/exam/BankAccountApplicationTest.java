e6c2e02d06284fcaa127ce97d5fb2a5764b099cf
unknown <abhilash.kamboj@gmail.com>
2021-11-26 20:55:47 +0100

    added failing test

--- src/test/java/com/pgr301/exam/BankAccountApplicationTest.java
+++ src/test/java/com/pgr301/exam/BankAccountApplicationTest.java
@@ -4,18 +4,16 @@ import com.pgr301.exam.model.Account;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.math.BigDecimal;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class BankAccountApplicationTest {
 
-
+        //Simple test that creates an account with an id, and checks if the id is correct
         @Test
         void checkAccountHasCorrectId(){
             Account account = new Account();
-            account.setId("3");
+            account.setId("1");
             assertEquals("1", account.getId());
         }
 }
